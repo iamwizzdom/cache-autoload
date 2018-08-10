bdba22b9593fce629fd6f769e68044b7719dda7c
wisdom.emenike <wisdom.emenike@imaxinacion.info>
2018-08-10 13:51:08 +0100

    enabled cache-autoload to find trait files.

--- cache-autoload.php
+++ cache-autoload.php
@@ -188,7 +188,8 @@ class CacheAutoload
         $count = count($tokens);
         for ($i = 2; $i < $count; $i++) {
             if (($tokens[$i - 2][0] == T_CLASS ||
-                    $tokens[$i - 2][0] == T_INTERFACE)
+                    $tokens[$i - 2][0] == T_INTERFACE ||
+                    $tokens[$i - 2][0] == T_TRAIT)
                 && $tokens[$i - 1][0] == T_WHITESPACE
                 && $tokens[$i][0] == T_STRING
             ) {
